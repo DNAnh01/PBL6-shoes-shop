90c5ed20541e36ccc906883ed0a56f43467a8282
Kin <kin092002@gmail.com>
2023-12-09 12:51:36 +0700

    xoa fr

--- frontend/.history/src/components/Layout/Footer/index_20231130194003.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./style-prefix.scss";
-import pay from "../../../images/payment.png";
-
-export default function Footer() {}
