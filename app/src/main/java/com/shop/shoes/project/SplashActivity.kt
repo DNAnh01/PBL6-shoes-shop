605ced10b33d42d6796f692eac3603f9a4d596c4
anhhthk2002 <daongocanh20052002@gmail.com>
2023-12-27 01:25:51 +0700

    fun splash

--- app/src/main/java/com/shop/shoes/project/SplashActivity.kt
+++ app/src/main/java/com/shop/shoes/project/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.shop.shoes.project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        lifecycleScope.launch {
+            delay(4000)
+            goToMain()
+        }
+    }
+
+    private fun goToMain(){
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
