d770528223bf6a6ad98127d1816fec46c60b647b
Đào Ngọc Anh <116966922+anhhtht@users.noreply.github.com>
2023-12-29 13:35:33 +0700

    Update ShippingAddressRequest.java

--- src/main/java/com/dnanh01/backend/request/ShippingAddressRequest.java
+++ src/main/java/com/dnanh01/backend/request/ShippingAddressRequest.java
@@ -2,55 +2,92 @@ package com.dnanh01.backend.request;
 
 public class ShippingAddressRequest {
 
+	private String firstName;
+
+    private String lastName;
+	
     private String streetAddress;
 
     private String city;
 
     private String state;
+    
+    private String mobile;
 
     private String zipCode;
 
-    public ShippingAddressRequest() {
+	public ShippingAddressRequest() {
 
-    }
+	}
 
-    public ShippingAddressRequest(String streetAddress, String city, String state, String zipCode) {
-        this.streetAddress = streetAddress;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-    }
+	public ShippingAddressRequest(String firstName, String lastName, String streetAddress, String city, String state,
+			String mobile, String zipCode) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.streetAddress = streetAddress;
+		this.city = city;
+		this.state = state;
+		this.mobile = mobile;
+		this.zipCode = zipCode;
+	}
 
-    public String getStreetAddress() {
-        return streetAddress;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setStreetAddress(String streetAddress) {
-        this.streetAddress = streetAddress;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public String getState() {
-        return state;
-    }
+	public String getStreetAddress() {
+		return streetAddress;
+	}
 
-    public void setState(String state) {
-        this.state = state;
-    }
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
 
-    public String getZipCode() {
-        return zipCode;
-    }
+	public String getCity() {
+		return city;
+	}
 
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+   
 
 }
