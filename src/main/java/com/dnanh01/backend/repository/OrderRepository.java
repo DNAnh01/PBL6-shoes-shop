55cc9ee555eb53c4555ff92375cd2199cc10cb48
Đào Ngọc Anh <116966922+anhhtht@users.noreply.github.com>
2023-12-30 22:28:37 +0700

    Update OrderRepository.java

--- src/main/java/com/dnanh01/backend/repository/OrderRepository.java
+++ src/main/java/com/dnanh01/backend/repository/OrderRepository.java
@@ -22,6 +22,10 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             "AND (o.orderStatus = 'PENDING' OR o.orderStatus = 'CONFIRMED' OR o.orderStatus = 'SHIPPED' OR o.orderStatus = 'DELIVERED')")
 	public List<Order> getUsersOrders(@Param("userId") Long userId);
 
+	@Query("SELECT o FROM Order o"
+			+ "WHERE o.orderStatus IN ('PENDING', 'CONFIRMED', 'SHIPPED', 'DELIVERED')")
+	public List<Order> getAllOrders();
+
 	public List<Order> findByUser(User user);
 
 	@Query("SELECT o FROM Order o " +
