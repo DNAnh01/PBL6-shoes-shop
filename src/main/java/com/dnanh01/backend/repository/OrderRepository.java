c47472845fc849e4adbb58dd8c4b7eae5d3bfe2c
Đào Ngọc Anh <116966922+anhhtht@users.noreply.github.com>
2024-01-03 14:18:46 +0700

    Update OrderRepository.java

--- src/main/java/com/dnanh01/backend/repository/OrderRepository.java
+++ src/main/java/com/dnanh01/backend/repository/OrderRepository.java
@@ -19,11 +19,11 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
 	@Query("SELECT o FROM Order o " +
             "WHERE o.user.id = :userId " +
-            "AND (o.orderStatus = 'PENDING' OR o.orderStatus = 'CONFIRMED' OR o.orderStatus = 'SHIPPED' OR o.orderStatus = 'DELIVERED' OR o.orderStatus = 'CANCELED')")
+            "AND (o.orderStatus = 'PENDING' OR o.orderStatus = 'CONFIRMED' OR o.orderStatus = 'CANCELED')")//OR o.orderStatus = 'SHIPPED' OR o.orderStatus = 'DELIVERED'
 	public List<Order> getUsersOrders(@Param("userId") Long userId);
 
 	
-	@Query("SELECT o FROM Order o WHERE o.orderStatus IN ('PENDING', 'CONFIRMED', 'SHIPPED', 'DELIVERED', 'CANCELED')")
+	@Query("SELECT o FROM Order o WHERE o.orderStatus IN ('PENDING', 'CONFIRMED', 'CANCELED')")//'SHIPPED', 'DELIVERED'
     public List<Order> getAllOrders();
 	
 	public List<Order> findByUser(User user);
